9fa34e83e0a747599554819d92f9dcf63fa69566
Romain Linkov <rlinkov@e-r12-p2.s19.be>
2020-01-07 18:51:21 +0100

    modification de printf

--- printf_exercice/ft_printf.c
+++ printf_exercice/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: rlinkov <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/29 16:38:39 by rlinkov           #+#    #+#             */
-/*   Updated: 2019/12/11 18:03:29 by rlinkov          ###   ########.fr       */
+/*   Updated: 2020/01/07 16:47:16 by rlinkov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 // --PRINTF : %[parameters][flags][width][.precision][length]type
 // si %% print % (doesn't accept any flags, width, precision, length fields)
 // * s'utilise pour la precision et prend en compte la variable suivante comme precision ex : printf("%.*d", 12, 13); -> 0000000000013 (ecrire 13 qvec precision de 12)
+/* ECRIT AVANT NOEL
 int		ft_printf(const char *str, ...)
 {
 	int			i;
@@ -34,4 +35,27 @@ int		ft_printf(const char *str, ...)
 
 	//-- va_arg(args, type) -- recupere l'argument et deplace le pointeur sur l'argument suivant, l'element renvoie est de type liste)
 	//-- remarque : les types sont int, double, char*, ... mais pas char ou float qui sont implicitement convertis en int et double
+} */
+
+int		ft_printf(const char *format, ...)
+{
+	va_list		args; //liste des arguments
+	
+	va_start(args, format); //fait pointer args sur str (qui est le dernier argument fixe de la fonction):
+	while (*format != '\0')
+	{
+		if (*format == '%')
+			format++;
+		if (*format == '%')
+			ft_putchar_fd ('%', 1);
+		else if (*format == 'd')
+		{
+			ft_putstr_fd(ft_itoa(va_arg(args, int)), 1);
+		}
+		else
+			ft_putchar_fd(*format, 1);
+		format++;
+	}
+	va_end(args);
+	return (0);
 }
--- printf_exercice/libftprintf.h
+++ printf_exercice/libftprintf.h
@@ -6,7 +6,7 @@
 /*   By: rlinkov <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/29 18:39:28 by rlinkov           #+#    #+#             */
-/*   Updated: 2019/12/11 13:49:29 by rlinkov          ###   ########.fr       */
+/*   Updated: 2020/01/07 18:26:30 by rlinkov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,22 @@
 
 # include "./libft/libft.h"
 # include <stdarg.h>
+
+typedef struct  s_flags
+{
+//	char        format;
+	void        *output;
+	int         length_output;
+	int			flags; // 0 ou - , si 0 et - , seul le - compte
+	int			width; // un entier ou une *
+
+	//int         asterix;
+	//int         dash;
+	//int         zero;
+	//int         point;
+	//int         precision; order -> flags (0 ou - ) -> width (int ou *) -> precision (int ou * apres un .) puis une conversion
+}               t_flags;
+
 
 int		ft_printf(const char *str, ...);
 
