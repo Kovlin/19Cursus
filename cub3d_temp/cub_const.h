324b2b8b3ad3a47edf70927f4d661b3dd9475a9e
Romain Linkov <rlinkov@mi-r6-p1.s19.be>
2020-10-15 17:50:22 +0200

    tmp

---
+++ cub3d_temp/cub_const.h
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cub_const.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rlinkov <rlinkov@student.s19.be>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/09/25 17:10:53 by rlinkov           #+#    #+#             */
+/*   Updated: 2020/10/15 13:57:13 by rlinkov          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CUB_CONST_H
+# define CUB_CONST_H
+
+# define NORTH 0
+# define SOUTH 1
+# define EAST 2
+# define WEST 3
+# define SPRITE 4
+
+# define DECAL 5 //decalage entre les textures et les couleurs du sol et du plafond dans e_count
+# define FLOOR 0
+# define CEILING 1
+# define R 0
+# define G 1
+# define B 2
+
+# define RES 7 //position de la resolution dans e_count
+# define X 0
+# define Y 1
+
+
+
+#endif
