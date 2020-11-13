82a908f5456cee3a48b73899dc31386eac6fbc8c
Romain Linkov <rlinkov@fu-r9-p8.s19.be>
2020-11-13 18:23:42 +0100

    texture Ok

---
+++ cub3d_text_ON/include/game_const.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   game_const.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rlinkov <rlinkov@student.s19.be>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/04 14:16:14 by rlinkov           #+#    #+#             */
+/*   Updated: 2020/11/13 16:23:33 by rlinkov          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GAME_CONST_H
+# define GAME_CONST_H
+
+# define SPEED 0.4
+# define SPEEDCAM 0.1
+# define W 13
+# define A 0
+# define S 1
+# define D 2
+# define R_ARROW 124
+# define L_ARROW 123
+# define ESC 53
+
+#endif
---
+++ cub3d_text_ON/src/utils/init_player.c
@@ -0,0 +1,78 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   init_player.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rlinkov <rlinkov@student.s19.be>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/04 14:01:47 by rlinkov           #+#    #+#             */
+/*   Updated: 2020/11/13 16:25:30 by rlinkov          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "cub3d.h"
+
+int     init_player_status(t_cube *cube, int i, int j)
+{
+    if (cube->map[i][j] == 'N')
+    {
+        cube->player->view[X] = 0.0;
+        cube->player->view[Y] = -1.0;
+        cube->player->plane[X] = 0.66;
+        cube->player->plane[Y] = 0;
+    }
+    else if (cube->map[i][j] == 'S')
+    {
+        cube->player->view[X] = 0;
+        cube->player->view[Y] = 1;
+        cube->player->plane[X] = -0.66;
+        cube->player->plane[Y] = 0;
+    }
+    else if (cube->map[i][j] == 'E')
+    {
+        cube->player->view[X] = 1;
+        cube->player->view[Y] = 0;
+        cube->player->plane[X] = 0;
+        cube->player->plane[Y] = 0.66;
+    }
+    else if (cube->map[i][j] == 'W')
+    {
+        cube->player->view[X] = -1;
+        cube->player->view[Y] = 0;
+        cube->player->plane[X] = 0;
+        cube->player->plane[Y] = -0.66;
+    }
+    else
+        return (0);
+    cube->map[i][j] = '0'; //eviter les problemes
+    cube->player->pos[X] = j + 0.5; //se positionner au milieu du carré pour lancer le rayon
+    cube->player->pos[Y] = i + 0.5;
+    return (1);
+}
+
+void    init_player(t_cube *cube)
+{
+    int i;
+    int j;
+    int ret;
+    
+    if (!(cube->player->pos = (double*)malloc(2 * sizeof(double))))
+        handle_error(ERR_MALLOC);
+    if (!(cube->player->view = (double*)malloc(2 * sizeof(double))))
+        handle_error(ERR_MALLOC);
+    if (!(cube->player->plane = (double*)malloc(2 * sizeof(double))))
+        handle_error(ERR_MALLOC);
+    i = 0;
+    ret = 0;
+    j = 0;
+    while ((i < cube->map_height) && ret == 0)
+    {
+        j = 0;
+        while(cube->map[i][j] && ret == 0)
+        {
+            ret = init_player_status(cube, i, j);
+            j++;
+        }
+        i++;
+    }
+}
