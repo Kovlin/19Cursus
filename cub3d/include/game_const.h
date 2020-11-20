beff7f9d4575f9f34a024ef8e6267c529a93941a
Romain Linkov <rlinkov@fu-r9-p8.s19.be>
2020-11-20 19:13:50 +0100

    Cub3d - Norm OK

---
+++ cub3d/include/game_const.h
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   game_const.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rlinkov <rlinkov@student.s19.be>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/04 14:16:14 by rlinkov           #+#    #+#             */
+/*   Updated: 2020/11/20 17:35:43 by rlinkov          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GAME_CONST_H
+# define GAME_CONST_H
+
+# define SPEED 0.27
+# define SPEEDCAM 0.05
+# define W 13
+# define A 0
+# define S 1
+# define D 2
+# define R_ARROW 124
+# define L_ARROW 123
+# define ESC 53
+# define UP 0
+# define DOWN 1
+# define LEFT 2
+# define RIGHT 3
+# define C_LEFT 4
+# define C_RIGHT 5
+# define KEYS 6
+
+#endif
