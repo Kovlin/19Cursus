fe8b6d2d6e8dae08ed532c9c72a96da2f9b1cb02
Romain Linkov <rlinkov@fu-r8-p8.s19.be>
2021-03-30 17:50:34 +0200

    test Libasm

--- libasm/ft_strlen.s
+++ libasm/ft_strlen.s
@@ -10,25 +10,15 @@ section .text:
 
 ft_strlen:
     mov rax,0 ; on initialise la longueur a 0 dans rcx
-    cmp rdi,0 ; on compare le premier caractere de rdi
+    cmp BYTE [rdi + rax],0 ; on compare le premier caractere de rdi
     je  exit  ; quitte le programme si premier caractere est 0
     jmp count ;si on a une chaine on va compter
 
 count:
     inc rax ;on incremente rcx
-    inc rdi ;on avance sur rdi
-    cmp rdi,0 
+    cmp BYTE [rdi + rax]i,0 
     je exit
     jmp count
 
 exit:
-    ; mov r9,rax ;on met la valeur de rax dans r9
-    ; mov rax,1 ;on prepare le write
-    ; mov r9, [r9 + 47] ;on met le chiffre en lettre
-    ; mov rdi,1
-    ; mov rsi,r9
-    ; mov rdx,1
-    ; syscall
-    ; mov rax, 60
-    ; syscall
     ret
