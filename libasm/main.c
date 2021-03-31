7f19b3041fc26fbbc643b211296cf15eee50ffd1
Romain Linkov <rlinkov@fu-r8-p8.s19.be>
2021-03-31 12:49:20 +0200

    test Libasm

--- libasm/main.c
+++ libasm/main.c
@@ -6,7 +6,7 @@
 /*   By: rlinkov <rlinkov@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/30 17:20:07 by rlinkov           #+#    #+#             */
-/*   Updated: 2021/03/30 17:52:55 by rlinkov          ###   ########.fr       */
+/*   Updated: 2021/03/31 12:49:18 by rlinkov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,25 @@ extern size_t ft_strlen(const char *str);
 int main()
 {
     printf("start....\n");
-    printf("original : %lu\n", strlen("bonjour"));
-    printf("%zu\n", ft_strlen("bonjour"));
+    printf("bonjour\n");
+    printf("original   : %lu\n", strlen("bonjour"));
+    printf("mon strlen :%zu\n\n", ft_strlen("bonjour"));
+
+    printf("");
+    printf("original   : %lu\n", strlen(""));
+    printf("mon strlen :%zu\n\n", ft_strlen(""));
+
+    printf("bonjourbonjour\n");
+    printf("original   : %lu\n", strlen("bonjourbonjour"));
+    printf("mon strlen :%zu\n\n", ft_strlen("bonjourbonjour"));
+
+    printf("b\n");
+    printf("original   : %lu\n", strlen("b"));
+    printf("mon strlen :%zu\n\n", ft_strlen("b"));
+
+    printf("\n");
+    printf("original   : %lu\n", strlen("\n"));
+    printf("mon strlen :%zu\n\n", ft_strlen("\n"));
     
     return(0);
 }
