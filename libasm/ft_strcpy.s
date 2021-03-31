fa6cf888c246dddde8e47b08b87ef51d5c98a435
Romain Linkov <rlinkov@fu-r8-p8.s19.be>
2021-03-31 15:36:08 +0200

    test Libasm

--- libasm/ft_strcpy.s
+++ libasm/ft_strcpy.s
@@ -17,11 +17,12 @@ ft_strcpy:
 copy:
     cmp BYTE    [rsi + rax],0           ; on compare la position actuelle dans la source et le caractère nul
     je          exit                    ; on saute à la fonction de sortie si on est au bout de la source
-    mov [rdi + rax],[rsi + rax] ; on copie le caractère de la source vers la destination
+    mov         rcx,[rsi + rax]         ; on place le caractère à copier dans un registre intermédiaire car on ne peut utiliser plus d'un opérande mémoire par instruction
+    mov         [rdi + rax],rcx         ; on copie le caractère dans la destination
     inc         rax                     ; on incrémente rax de 1
     jmp         copy                    ; on retourne au début de la fonction copy
 
 exit:
-    mov BYTE    [rdi + rax], 0x0        ; on force le \0 la fin de la destination
+    mov BYTE    [rdi + rax], 0          ; on force le \0 la fin de la destination
     mov         rax,rdi                 ; la valeur de retour est contenue dans rax, on y met donc la chaîne destination
     ret                                 ; met fin à la fonction, la valeur contenue dans rax est renvoyée par la fonction
