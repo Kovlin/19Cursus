bf024c2084c7111f1e2a3883a25cffc729fdc9cf
Romain Linkov <rlinkov@fu-r8-p8.s19.be>
2021-03-31 12:40:39 +0200

    test Libasm

--- libasm/ft_strlen.s
+++ libasm/ft_strlen.s
@@ -3,23 +3,23 @@
 ; To assemble and run:
 ;
 ;     nasm -felf64 ft_strlen.s && ld ft_strlen.o && ./a.out
+;     By rlinkov@student.s19.be
 ; ----------------------------------------------------------------------------------------
 
 section .text:
     global ft_strlen
 
 ft_strlen:
-    mov rax,0 ; on initialise la longueur a 0 dans rcx
-    cmp BYTE [rdi + rax],0 ; on compare le premier caractere de rdi
-    je  exit  ; quitte le programme si premier caractere est 0
-    jmp count ;si on a une chaine on va compter
+    mov rax,0       ; on utilise rax qui permet de renvoyer une valeur lorsque ret est utilsé et on l'initialise à 0
+                    ; il servira de longueur de la chaîne de caractère
+    jmp calc_length ; on saute vers la fonction de calcul de la longueur
 
-count:
-    inc rax ;on incremente rcx
-    cmp BYTE [rdi + rax],0 
-    je exit
-    jmp count
+calc_length:
+    cmp BYTE    [rdi + rax],0   ; on compare le premier octet de RDI (dans lequel est stcoké le premier paramètre
+                                ; passé à la fonction) avec 0 pour verifier si la chaîne est terminée
+    je          exit            ; si la comparaison précédente est une égalité on saute vers la fonction exit
+    inc         rax             ; on incrémente rax de 1
+    jmp         calc_length     ; on saute au début de calc_length (on "boucle" dessus)
 
 exit:
-    ret
-;fonctionne mais a rendre plus clean
+    ret ; met fin a la fonction, la valeur contenue dans rax est renvoyée par la fonction
