dbdf99e3ea617298d0520990e48aeef6a5eaf884
Romain Linkov <rlinkov@fu-r8-p8.s19.be>
2021-03-31 18:01:17 +0200

    test Libasm

--- libasm/ft_strcmp.s
+++ libasm/ft_strcmp.s
@@ -14,16 +14,20 @@ ft_strcmp:
     jmp compare ; on saute à la fonction de comparaison
 
 compare:
-    movzx r10,BYTE [rdi + rax] ; on place le caractère actuel de la chaîne 1 dans le registre r10
-    movzx r11,BYTE [rsi + rax] ; on place le caractère actuel de la chaîne 2 dans le registre r11
-    cmp r10,0                  ; on vérifie si on est en bout de chaîne 1
-    je  substraction           ; si on est en bout de chaîne on saute à la fonction substraction
-    cmp r11,0                  ; on vérifie si on est en bout de chaîne 2
-    je  substraction           ; si on est en bout de chaîne on saute à la fonction substraction
-    cmp r10,r11                ; on compare le caractère actuel des deux chaînes
-    jne substraction           ; si ils sont différents on saute à la fonction substraction
-    inc rax                    ; on incrémente rax de 1
-    jmp compare                ; on saute au début de la fonction compare
+    movzx r10,BYTE [rdi + rax]  ; on place le caractère actuel de la chaîne 1 dans le registre r10
+    movzx r11,BYTE [rsi + rax]  ; on place le caractère actuel de la chaîne 2 dans le registre r11
+                                ; ne fonctionne pas avec mov, il faut utiliser movzx,
+                                ; movzx va placer la valeur du byte à l'adresse [rdi/rsi + rax]
+                                ; et va ensuite remplir le reste du registre de 0, on évite ainsi
+                                ; certaines erreurs possible les opérations de comparaison
+    cmp r10,0                   ; on vérifie si on est en bout de chaîne 1
+    je  substraction            ; si on est en bout de chaîne on saute à la fonction substraction
+    cmp r11,0                   ; on vérifie si on est en bout de chaîne 2
+    je  substraction            ; si on est en bout de chaîne on saute à la fonction substraction
+    cmp r10,r11                 ; on compare le caractère actuel des deux chaînes
+    jne substraction            ; si ils sont différents on saute à la fonction substraction
+    inc rax                     ; on incrémente rax de 1
+    jmp compare                 ; on saute au début de la fonction compare
 
 
 substraction:
