8e61106fe47dede628a58d2032d35b26ff79b4d4
Romain Linkov <rlinkov@fu-r8-p8.s19.be>
2021-03-31 14:19:24 +0200

    test Libasm

--- libasm/ft_strcpy.s
+++ libasm/ft_strcpy.s
@@ -22,6 +22,5 @@ copy:
     jmp         copy                    ; on retourne au début de la fonction copy
 
 exit:
-    mov r10,0
-    mov [rdi + rax], r10           ; on force le \0 la fin de la destination
+    mov BYTE [rdi + rax], 0x0           ; on force le \0 la fin de la destination
     ret                                 ; met fin à la fonction, la valeur contenue dans rax est renvoyée par la fonction
