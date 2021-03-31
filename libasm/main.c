94aa2664b30ad0d46f418a87c92564e913b86105
Romain Linkov <rlinkov@fu-r8-p8.s19.be>
2021-03-31 14:12:50 +0200

    test Libasm

--- libasm/main.c
+++ libasm/main.c
@@ -6,7 +6,7 @@
 /*   By: rlinkov <rlinkov@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/30 17:20:07 by rlinkov           #+#    #+#             */
-/*   Updated: 2021/03/31 14:02:27 by rlinkov          ###   ########.fr       */
+/*   Updated: 2021/03/31 14:10:11 by rlinkov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,30 +14,59 @@
 #include <stdio.h>
 #include <string.h>
 
-size_t ft_strlen(const char *str);
+size_t  ft_strlen(const char *str);
+char	*ft_strcpy(char *dst, const char *src);
 
 int main()
 {
-    printf("start....\n");
+
+    char    *tmp;
+
+	printf("===========================\n");
+	printf("         FT_STRLEN         \n");
+	printf("===========================\n\n");
+
     printf("bonjour\n");
     printf("original   : %lu\n", strlen("bonjour"));
-    printf("mon strlen :%zu\n\n", ft_strlen("bonjour"));
+    printf("mon strlen : %zu\n\n", ft_strlen("bonjour"));
 
     printf("");
     printf("original   : %lu\n", strlen(""));
-    printf("mon strlen :%zu\n\n", ft_strlen(""));
+    printf("mon strlen : %zu\n\n", ft_strlen(""));
 
     printf("bonjourbonjour\n");
     printf("original   : %lu\n", strlen("bonjourbonjour"));
-    printf("mon strlen :%zu\n\n", ft_strlen("bonjourbonjour"));
+    printf("mon strlen : %zu\n\n", ft_strlen("bonjourbonjour"));
 
     printf("b\n");
     printf("original   : %lu\n", strlen("b"));
-    printf("mon strlen :%zu\n\n", ft_strlen("b"));
+    printf("mon strlen : %zu\n\n", ft_strlen("b"));
 
     printf("\n");
     printf("original   : %lu\n", strlen("\n"));
-    printf("mon strlen :%zu\n\n", ft_strlen("\n"));
+    printf("mon strlen : %zu\n\n", ft_strlen("\n"));
+
+
+    printf("===========================\n");
+	printf("         FT_STRCPY         \n");
+	printf("===========================\n\n");
+    
+    printf("hello");
+    
+	printf("strcpy : |%s|\n", strcpy(tmp, "world"));
+	printf("ft_strcpy : |%s|\n\n", ft_strcpy(tmp, "world"));
+    
+    printf("strcpy : |%s|\n", strcpy(tmp, "hello world"));
+	printf("ft_strcpy : |%s|\n\n", ft_strcpy(tmp, "hello world"));
+
+	printf("strcpy : |%s|\n", strcpy(tmp, ""));
+	printf("ft_strcpy : |%s|\n\n", ft_strcpy(tmp, ""));
+
+	printf("strcpy : |%s|\n", strcpy(tmp, "enjjweiofhfiuhgeiwoijqbvwjfeojefkj"));
+	printf("ft_strcpy : |%s|\n\n", ft_strcpy(tmp, "enjjweiofhfiuhgeiwoijqbvwjfeojefkj"));
+
+	printf("strcpy : |%s|\n", strcpy(tmp, "Ceci est un test"));
+	printf("ft_strcpy : |%s|\n\n", ft_strcpy(tmp, "Ceci est un test"));
     
     return(0);
 }
