90cc64ecb8180b93a163caadcc1997520a5d0671
Romain Linkov <rlinkov@MacBook-Pro-de-Romain.local>
2021-04-01 13:46:20 +0200

    avancement Libasm

--- libasm/ft_write.s
+++ libasm/ft_write.s
@@ -13,6 +13,13 @@ section .text:
     global ft_write
 
 ft_write:
-    mov rax,1 ; le code de l'appel système write est 1
-    syscall
-    ret
+    mov rax,1       ; le code de l'appel système write est 1
+    syscall         ; on appelle la fonction associée au code passé à rax
+    cmp rax,0       ; on évalue rax par rapport à 0
+    jl  exit_error  ; si rax est inférieur à 0, il y a eu une erreur
+                    ; on saute à la fonction de gestion de l'erreur
+    ret             ; si tout ce passe bien, on retourne à la fonction d'appel
+
+exit_error:
+    mov rax,-1      ; on met rax à -1 car c'est la valeur à retourner en cas d'erreur
+    ret             ; on retourne à la fonction d'appel
