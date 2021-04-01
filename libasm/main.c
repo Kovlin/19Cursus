339ed6c6b65444461a1e14467f7def7993da44c6
Romain Linkov <rlinkov@MacBook-Pro-de-Romain.local>
2021-04-01 14:11:42 +0200

    avancement Libasm

--- libasm/main.c
+++ libasm/main.c
@@ -6,7 +6,7 @@
 /*   By: rlinkov <rlinkov@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/30 17:20:07 by rlinkov           #+#    #+#             */
-/*   Updated: 2021/04/01 14:09:49 by rlinkov          ###   ########.fr       */
+/*   Updated: 2021/04/01 14:11:33 by rlinkov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -152,6 +152,7 @@ int main()
 	printf("errno : %s %d\n", strerror(errno), errno);
 	
    	write(0xff, "hello!\n", 7);
+	printf("errno : %s %d\n", strerror(errno), errno);
 	printf("errno : %s %d\n", strerror(errno), errno);
 	ft_write(0xff, "hello!\n", 7);
 	printf("\n");
