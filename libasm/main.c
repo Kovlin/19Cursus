1fcbc878f5595ae402c81d4f93392ba9ae3edd68
Romain Linkov <rlinkov@MacBook-Pro-de-Romain.local>
2021-04-07 18:22:58 +0200

    avancement Libasm

--- libasm/main.c
+++ libasm/main.c
@@ -6,7 +6,7 @@
 /*   By: rlinkov <rlinkov@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/30 17:20:07 by rlinkov           #+#    #+#             */
-/*   Updated: 2021/04/07 18:19:04 by rlinkov          ###   ########.fr       */
+/*   Updated: 2021/04/07 18:22:50 by rlinkov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -214,13 +214,13 @@ int main()
 		bzero(buffer, 100);
 		printf("read return    : %zd\n", read(-3, buffer, 25));
 		printf("errno : %s %d\n", strerror(errno), errno);
-		//printf("BUFFER : %s\n",buffer);
+		printf("BUFFER : %s\n",buffer);
 		lseek(fd, 0, SEEK_SET);
 		buffer[25] = 0;
 		printf("ft_read return : %zd\n", ft_read(-3, buffer, 25));
 		printf("errno : %s %d\n", strerror(errno), errno);
 		buffer[25] = 0;
-		//printf("BUFFER : %s\n",buffer);
+		printf("BUFFER : %s\n",buffer);
 		lseek(fd, 0, SEEK_SET);
 		printf("\n");
 
@@ -262,7 +262,6 @@ int main()
 	buffer[buffer_size] = 0;
 	printf("input ft_read : |%s|\n",buffer);
 	printf("\n");
-
 	free(buffer);
 	
     return(0);
