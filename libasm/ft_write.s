276400e40a393b7c216d7089ac2ed021691fdeb9
Romain Linkov <rlinkov@MacBook-Pro-de-Romain.local>
2021-04-07 15:46:56 +0200

    avancement Libasm

--- libasm/ft_write.s
+++ libasm/ft_write.s
@@ -20,16 +20,21 @@ ft_write:
     ret             ; si tout ce passe bien, on retourne à la fonction d'appel
 
 exit_error:
-    neg rax         ; la valeur opposée à errno se retrouve dans rax (sous Linux)
-                    ; on fait donc rax *-1 pour récupérer sa valeur
-
-
-    mov r12, rax ; on met le contenu de rax dans r12
-    push rax; on push le contenu de rax sur la stack
-    call    __errno_location ;rax pointe mtn sur errno
-    mov rax , r12 ; on met le contenu de r12 dans errno
-    pop rax ; on recupere le contenu du haut de la stack dans rax (on repointe dessus)
-            ;errno a changé et on renvoi le -1 apres
+    neg     rax                 ; la valeur opposée à errno se retrouve dans rax (sous Linux)
+                                ; on fait donc rax *-1 pour récupérer sa valeur
+    push    rax                 ; on push la valeur de rax sur la stack
+    call    __errno_location    ; on appelle ___errno_location, on récupère donc le pointeur
+                                ; de errno dans rax
+    pop     [rax]               ; on récupère la valeur push de rax dans [rax],
+                                ; on met donc la valeur de rax push auparavant là où pointe errno
+    
+    ; mov     [rax],
+    ; mov r12, rax ; on met le contenu de rax dans r12
+    ; push rax; on push le contenu de rax sur la stack
+    ; call    __errno_location ;rax pointe mtn sur errno
+    ; mov rax , r12 ; on met le contenu de r12 dans errno
+    ; pop rax ; on recupere le contenu du haut de la stack dans rax (on repointe dessus)
+    ;         ;errno a changé et on renvoi le -1 apres
 
 
     mov rax,-1      ; on met rax à -1 car c'est la valeur à retourner en cas d'erreur
