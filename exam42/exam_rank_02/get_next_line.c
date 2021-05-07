4e7b177f9083497b0e63e210f156821f6a8ffe2a
Romain Linkov <rlinkov@shi-r11-p1.s19.be>
2021-05-07 16:41:30 +0200

    valid exam version gnl

--- exam42/exam_rank_02/get_next_line.c
+++ exam42/exam_rank_02/get_next_line.c
@@ -1,138 +1,47 @@
 #include "get_next_line.h"
-
-int    strlen_eol(char *s)
+int ft_strlen(char *s)
 {
-    int length;
-
-    length = 0;
-    while (s && s[length] && s[length] != '\n')
-        length++;
-    return (length);
+    int i;
+    i = 0;
+    while (s && s[i])
+        i++;
+    return (i);
 }
-
-char *strjoin_to_eol(char *line, char *buffer)
+char *add_char(char *s, char c)
 {
     char *new_line;
-    int i;
-    int j;
-
-    if (!(new_line = malloc(strlen_eol(line) + strlen_eol(buffer) + 1)))
-        return (NULL);
+    int    i;
+    new_line = malloc(ft_strlen(s) + 1 + 1);
     i = 0;
-    while (line[i] != 0 && line)
+    while (s && s[i])
     {
-        new_line[i] = line[i];
+        new_line[i] = s[i];
         i++;
     }
-    j = i;
-    i = 0;
-    while (buffer && buffer[i] != 0 && buffer[i] != '\n')
-    {
-        new_line[i + j] = buffer[i];
-    }
-    new_line[i + j] = '\0';
-    free(line);
+    new_line[i] = c;
+    new_line[i + 1] = '\0';
+    free(s);
     return (new_line);
 }
-//char    *strjoin_to_eol(char *s1, char *buf)
-//{
-//    char    *tab;
-//    int        i;
-//    int        j;
-//
-//    i = 0;
-//    j = 0;
-//    if (!(tab = malloc(strlen_eol(s1) + strlen_eol(buf) + 1)))
-//        return (NULL);
-//    while (s1 && s1[j])
-//        tab[i++] = s1[j++];
-//    while (buf && *buf && *buf != '\n')
-//        tab[i++] = *buf++;
-//    tab[i] = 0;
-//    free(s1);
-//    return (tab);
-//}
-int strchr_pos(char *line, char c)
+
+int get_next_line(char **line)
 {
-    int pos;
-
-    pos = 0;
-    while (line[pos] && line[pos] != c)
-    {
-        pos++;
-    }
-    if (line[pos] != c)
-    {
-        return (-1);
-    }
-    return (pos);
-}
-
-void ft_bzero(char *s, int size)
-{
-    int i;
-    i = 0;
-    while (i < size)
-    {
-        s[i] = 0;
-        i++;
-    }
-}
-
-
-//int get_next_line(char **line)
-//{
-//    static char    buffer[BUFFER_SIZE + 1];
-//    int ret;
-//
-//    if (!line) //si la ligne est vide, on y colle le buffer
-//    {
-//        if (!(*line = strjoin_to_eol(NULL, buffer)))
-//            return (-1);
-//    }
-//    ret = 1;
-//    while (strchr_pos(*line, '\n') == -1 && ret && ret != -1)
-//    {
-//        ft_bzero(buffer, BUFFER_SIZE + 1);
-//        ret = read(0, buffer, BUFFER_SIZE);
-//        if (!(*line = strjoin_to_eol(*line, buffer)))
-//            return (-1);
-//    }
-//    int i = 0;
-//    ret = strchr_pos(buffer, '\n') + 1;
-//    if (buffer[0] == 0)
-//        return (0);
-//    while (buffer[ret] != '\0' && ret)
-//    {
-//        buffer[i] = buffer[ret];
-//        i++;
-//        ret++;
-//    }
-//    buffer[i] = 0;
-//    return (1);
-//}
-int        get_next_line(char **line)
-{
-    static char        buf[BUFFER_SIZE + 1];
-    int                ret;
-    int                i;
-    
-    if (!line || !(*line = strjoin_to_eol(NULL, buf)))
+    char buff[1];
+    int ret;
+    buff[0] = '\0';
+    if (!line || !(*line = malloc(1)))
         return (-1);
     ret = 1;
-    while (strchr_pos(buf, '\n') == -1 && ret && ret != -1)
+    *line[0] = '\0';
+    while (buff[0] != '\n' && ret && ret != -1)
     {
-        ft_bzero(buf, BUFFER_SIZE + 1);
-        ret = read(0, buf, BUFFER_SIZE);
-        if (!(*line = strjoin_to_eol(*line, buf)))
-            return (-1);
+        ret = read(0, buff, 1);
+        if (buff[0] != '\n')
+            *line = add_char(*line, buff[0]);
+        else
+            *line = add_char(*line, '\0');
     }
-    i = 0;
-    ret = strchr_pos(buf, '\n') + 1;
-    if (buf[0] == 0)
+    if (buff[0] == '\0')
         return (0);
-    while (buf[ret] != '\0' && ret)
-        buf[i++] = buf[ret++];
-    buf[i] = 0;
     return (1);
 }
--- exam42/exam_rank_02/get_next_line.h
+++ exam42/exam_rank_02/get_next_line.h
@@ -2,6 +2,5 @@
 # define GET_NEXT_LINE_H
 # include <unistd.h>
 # include <stdlib.h>
-# define BUFFER_SIZE 32
 int	get_next_line(char **line);
 #endif
