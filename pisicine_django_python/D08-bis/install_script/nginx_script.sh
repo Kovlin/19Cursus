029b06b1d30bad19e6e6f1987e84c25ebba97553
Romain Linkov <rlinkov@fu-r5-p5.s19.be>
2021-06-25 22:32:38 +0200

    D08

---
+++ pisicine_django_python/D08-bis/install_script/nginx_script.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+S_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+cp "$S_DIR/nginx_conf" ~/.brew/etc/nginx/servers/d08
+
+mkdir ~/d08/ssl
+dir=~/d08/ssl
+keyfile=$dir/${1/"*."/star.}.key
+cert=$dir/${1/"*."/star.}.crt
+
+openssl req -new -x509 -days 10000 -sha1 -newkey rsa:1024 \
+       -nodes -keyout $keyfile -out $cert -subj /O=$1/OU=/CN=$1
+
+cd ..
+python3 manage.py makemigrations
+python3 manage.py migrate
+python3 manage.py collectstatic
+
+brew services restart nginx
+
+open "http://127.0.0.1"
+open "https://127.0.0.1"
+
+gunicorn -c install_script/gunicorn.py d08.wsgi
